e3aa04852455150437da8e540f6702d5c80b4e1d
capecchi <william.capecchi@gmail.com>
2021-12-31 07:58:31 -0600

    added numbering to grandmas questions (and pathing for working on work laptop)

--- grandmas_questions.py
+++ grandmas_questions.py
@@ -1,21 +1,27 @@
 import os
 
-import numpy as np
+if os.path.exists('C:/Users/Owner/Dropbox'):
+	direc = 'C:/Users/Owner/Dropbox/grandmas_questions/'
+elif os.path.exists('C:/Users/wcapecch/Dropbox'):
+	direc = 'C:/Users/wcapecch/Dropbox/grandmas_questions/'
+else:
+	print('no good directory found-- where are you??')
 
-direc = 'C:/Users/Owner/Dropbox/grandmas_questions/'
 files = [f for f in os.listdir(direc) if f.startswith('pagesource_')]
 
 lines2write = []
+num = 1
 for file in files:
-    print(f'analyzing {file}')
-    lines2write.append('<b>'+file.replace('pagesource_', '').replace('.txt', '') + '</b><br><br>')
-    with open(direc + file, 'r') as fread:
-        lines = fread.readlines()
-        for q in lines[0].split('"headline flex suggestion">')[1:]:
-            newline = q.split('</div>')[0].replace('&#39;', '\'')+'<br>'
-            if newline not in lines2write:
-                lines2write.append(newline)
-    lines2write.append('<br><br>')
+	print(f'analyzing {file}')
+	lines2write.append('<b>' + file.replace('pagesource_', '').replace('.txt', '') + '</b><br><br>')
+	with open(direc + file, 'r') as fread:
+		lines = fread.readlines()
+		for q in lines[0].split('"headline flex suggestion">')[1:]:
+			newline = q.split('</div>')[0].replace('&#39;', '\'') + '<br>'
+			if newline not in lines2write:
+				lines2write.append(f'{num}. {newline}')
+				num += 1
+	lines2write.append('<br><br>')
 
 with open(direc + 'grandmas_questions.html', 'w') as fwrite:
-    fwrite.writelines(lines2write)
+	fwrite.writelines(lines2write)
